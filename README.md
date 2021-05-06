22e6d141da74cd03143330e71dae69c3828af33f
Luis Cambero <camberopiqueras@gmail.com>
2021-05-06 11:47:15 +0200

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CORD-19-NLP
 Topic modelling over CORD-19 database
+
+## Install
+```
+source install.sh
+```
---
+++ install.sh
@@ -0,0 +1,24 @@
+# Download the data
+wget https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases/cord-19_2021-04-26.tar.gz
+# Unzip the file
+tar -xf cord-19_2021-04-26.tar.gz
+
+# Rename the data folder
+mv 2021-04-26 data
+# Remove the zip file
+rm cord-19_2021-04-26.tar.gz
+
+PWD=`pwd`
+activate () {
+    . $PWD/venv/bin/activate
+}
+
+# Create the virtual environment 
+python3 -m venv venv
+activate
+
+# Install the dependencies
+pip install -r requirements.txt
+pip install spacy
+pip install --upgrade spacy
+python -m spacy download en_core_web_md
